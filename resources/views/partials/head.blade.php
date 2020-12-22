179b84704d5806c7b85505194fb3fbaa3a79ddb2
Filip Pacurar <filip@pacurar.net>
2020-12-22 14:19:28 +0200

    wip

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,7 +7,7 @@
 
     <link rel="shortcut icon" href="{{ public_url('images/favicon.ico') }}" type="image/vnd.microsoft.icon"/>
     <link rel="apple-touch-icon-precomposed" href="{{ public_url('images/apple-touch-icon-precomposed.png') }}">
-    <link rel="stylesheet" href="https://use.typekit.net/rqd7nkn.css">
+{{--    <link rel="stylesheet" href="https://use.typekit.net/rqd7nkn.css">--}}
     <link href="https://fonts.googleapis.com/css?family=Sigmar+One&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <link rel="preload" as="font" href="https://pacurar.net/wp-content/themes/pacurar2020/resources/fonts/bariol_regular-webfont.woff" type="font/woff" crossorigin="anonymous">
