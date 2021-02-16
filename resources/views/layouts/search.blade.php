ab878a685e3d0f48fa1d24b76bba302954c85da1
Filip Pacurar <filip@pacurar.net>
2021-02-16 17:25:59 +0200

    s. to

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -16,7 +16,10 @@ $cls = Cookie::get('containerfull', 0) == 1 ? 'containerfull' : 'container';
 @endphp
 
 <div class="{{ $cls }} maincontainer @yield('containerStyles')" x-data="{gem: false}" style="z-index: 1;"
-@creak.window="$refs.creak.play()">
+@creak.window="new Howl({
+  src: ['{{ public_url('fun/wood.mp3') }}'],
+  autoplay: true,
+})">
     <div class="my-8 w-full border-2 shadow-box shadow-button border-black p-4 bg-white flex-1 flex flex-col @yield('extraClassesContent')"
          style="">
         @include('partials.header')
