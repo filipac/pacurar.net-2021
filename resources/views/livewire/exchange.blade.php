6fe331e04b218f0a560b9a1d90f104c35e278ee3
Filip Pacurar <filip@pacurar.net>
2021-05-26 22:51:26 +0300

    wip

--- resources/views/livewire/exchange.blade.php
+++ resources/views/livewire/exchange.blade.php
@@ -2,7 +2,7 @@
 <div x-data="{ amount: @entangle('amount') }">
     <div>In order to send me</div>
     <div class="flex justify-center text-4xl">
-        <div contenteditable x-on:blur="amount = $event.target.innerHTML" contenteditable="true">{{$amount}}</div>
+        <div contenteditable x-on:blur="amount = $event.target.innerText" contenteditable="true">{{$amount}}</div>
         <div class="ml-2">EUR</div>
     </div>
     <div>equivalent in EGold</div>
