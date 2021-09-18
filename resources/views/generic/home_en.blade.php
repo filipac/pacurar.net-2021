a5790e86cfaa9cdfbbad8fd2a0994f188cc4bac0
Filip Pacurar <filip@pacurar.net>
2021-09-18 16:43:56 +0300

    write daily challenge wip

--- resources/views/generic/home_en.blade.php
+++ resources/views/generic/home_en.blade.php
@@ -1,7 +1,10 @@
- <div class="flex flex-col md:flex-row">
+<div class="flex justify-center text-center pb-8">
+    <h1 class="strikethroughLink text-white text-5xl">Hello there 👋!</h1>
+</div>
+<div class="flex flex-col md:flex-row">
         <div class="bg-white w-full md:w-1/3">
-            <div class="p-2 text-center">
-                <h1>Hello there 👋!</h1>
+            <div class="p-2 flex">
+                <h1 class="strikethroughLink inverse">#aboutMe</h1>
             </div>
             <div @touchstart="$refs.hi.play()" @touchend="$refs.hi.pause();" @mouseover="$refs.hi.play()" @mouseout="$refs.hi.pause();">
                 <video poster="{{get_stylesheet_directory_uri().'/resources/hi_poster.jpg'}}" muted x-ref="hi" loop style="object-fit: cover">
@@ -14,11 +17,20 @@
             </div>
         </div>
         <div class="bg-yellow w-full md:w-2/3 md:ml-4 mt-4 md:mt-0 flex flex-col p-4">
-            <div class="text-2xl w-full text-center">Paid sponsorship <small class="text-xs block mb-2 md:mb-0 md:inline">(paid by me, of course)</small></div>
+            <div class="w-full flex justify-center">
+                <div class="text-2xl strikethroughLink black">#paidSponsorship <small class="text-xs">(paid by me, of course)</small>
+                </div>
+            </div>
             <div>
                 <p>Hey, <span class="text-2xl font-bold">you</span>! Yes, I'm talking about you! Pay attention for a few minutes here.</p>
                 <p class="mt-2">There you go, no that my awesome marketing techniques worked and I have your attention, let's talk serious business.</p>
                 <p class="mt-2">I've been a full time programmer for more that 10 years. I've been a geek since general school and highschool and in my free time I exercised web programming, so I gained experience since sort of early in my life.</p>
+                <p class="mt-2">
+                    Currently I am working full-time with a super Romanian business called <a href="https://www.arnia.com/"
+                                                                                              class="strikethroughLink inverse black">Arnia
+                        Software</a>.
+                    This means that I don't have extra time for other client projects in this period. Nevertheless, here are some of the technologies I work with:
+                </p>
                 <p class="mt-4">
 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#php</span>
 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#laravel</span>
@@ -34,18 +46,51 @@
 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#bootstrap</span>
 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#javascript</span>
 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#mysql</span>
-<span class="text-base">, those are just a few of the techhologies that you have 10% chances of knowing about if you are not in the programming world, but I know them to some extent and I work with those daily.
-There is no perfect solution though, that's why we can look for the best solution togheter for your project.
-</span>
 </p>
-<p class="mt-2">I work for clients all over the world, Canada, USA and of course, my home country Romania. So you're all welcomed.</p>
-                <p class="mt-2">If you want me to make an IT project with you that is not <strong>boring</strong>, we can get in touch on <a href="mailto:filip@pacurar.dev">filip@pacurar.dev</a>, let's talk.</p>
+                <p class="mt-2">
+                    If you are not a technical person, there are little-to-none chances you know some of those above.
+                    What I can tell you from my 10+ years experience is that there is no perfect solution nor technology.
+                    Each project has it's own requirements so I am never refusing an opportunity to learn new techs.
+                </p>
+                <p class="mt-2">I work for clients all over the world, Canada, USA and of course, my home country Romania.</p>
+                <p class="mt-2">If you want me to make an IT project with you that is not <strong>boring</strong>, we can get in touch on
+                    <a href="mailto:filip@pacurar.dev">filip@pacurar.dev</a>, let's talk.
+                    Please note that I am not available for new programming projects, all I can offer right now is IT consultancy.</p>
                 <p class="mt-4 py-12 text-center w-fulL">
-<a href="/about-me/it-consultancy" class="btn-cta-consultanta">I want IT consultancy</a>
-</p>
+                    <a href="/about-me/it-consultancy" class="btn-cta-consultanta">I want IT consultancy</a>
+                </p>
             </div>
         </div>
     </div>
+<div class="bg-purple-300 w-full p-3 my-4 shadow-box">
+    <h3 class="text-xl"><span class="font-bold strikethroughLink">#writeDaily</span> challenge ✍️</h3>
+    <p>I've challenged myself in September 2021 to try and write something daily on this blog, as often as I can.</p>
+    <p>The rulse for this challenge are the following:</p>
+    <ul class="list-disc list-inside">
+        <li>A streak begins when I've published something on a consecutive day</li>
+        <li>The streak starts at 2. For example, if I write a post on September 18th, it is currently 0, but if I publish another article the next day
+        (Sep. 19th), the streak automatically jumps to 2</li>
+        <li>The streak is automatically reset where the last post is not for the current or previous day</li>
+        <li>Public or private posts count towards the streak. Private posts are still published on this blog but only visible to me</li>
+        <li>It does not matter if the post is in english or romanian</li>
+    </ul>
+    <p class="mt-2">The source code where I calculate this streak-thing is <a
+            href="https://github.com/filipac/pacurar.net-2021/blob/master/app/Jobs/CalculateStreak.php" target="_blank"
+            class="strikethroughLink inverse">here</a>
+    </p>
+    <div class="flex flex-col lg:flex-row w-full border-8 border-yellow mt-4">
+        <div
+            class="flex-1 flex flex-col items-center justify-center p-10 border-b-8 lg:border-r-8 lg:border-b-0 border-yellow">
+            <div class="text-2xl font-bold">Current streak</div>
+            <div
+                class="text-6xl font-bold mt-4">{{ ($streak = get_option('current_daily_streak_100d')) }} {{ $streak == 0 ? '😢' : '' }} {{ $streak != 0 && $streak < 5 ? '😄' : '' }} {{ $streak != 0 && $streak > 5 ? '💪' : '' }}</div>
+        </div>
+        <div class="flex-1 flex flex-col items-center justify-center p-10">
+            <div class="text-2xl font-bold">My best streak</div>
+            <div class="text-6xl font-bold mt-4">{{ get_option('best_daily_streak_100d') }} 🔥</div>
+        </div>
+    </div>
+</div>
     <a class="block w-full bg-green-200 my-4 p-2 py-4 shadow-box hover:shadow-boxhvr text-center font-bold" href="/blog">
         Read the blog, probabily that's why you are here.
     </a>
@@ -53,7 +98,9 @@ There is no perfect solution though, that's why we can look for the best solutio
         <div class="flex flex-col w-full md:w-2/3">
             <div class="bg-pink-200">
                 <div class="p-2 pb-4">
-                    <h1>Easter-egg hunt!</h1>
+                    <div class="flex">
+                        <h1 class="strikethroughLink inverse">#easterEggHunt!</h1>
+                    </div>
                     <p>Because this is not a boring blog, I've hidden a few interactive bits around here. Are you up to finding all of them? To be fair, I don't even know how many are there, because whenever I get a cool idea, I code it directly without thinking.</p>
                     <p class="mt-2">Let me give you an example hint: on this page you can see me waving my hand to you.</p>
                     <p class="mt-4">Saw it? This is a simple thing, but here are more well hidden things on all those pages.</p>
@@ -64,7 +111,9 @@ There is no perfect solution though, that's why we can look for the best solutio
             </div>
             <div class="bg-purple-300 mt-4">
                 <div class="p-2 pb-4 text-center">
-                    <div class="font-bold text-2xl">You can follow me on:</div>
+                    <div class="flex justify-center">
+                        <div class="font-bold text-2xl strikethroughLink">#followMe:</div>
+                    </div>
                     <div class="font-bold text-xs">(even if you're FBI or something, I got nothing to hide)</div>
                     <div class="flex flex-col md:flex-row items-center justify-center mt-4">
                         <div>
@@ -96,27 +145,38 @@ There is no perfect solution though, that's why we can look for the best solutio
                 </div>
             </div>
             <div class="bg-green-400 mt-4 p-4 flex-1">
-                <div class="font-bold text-4xl">Copyright...</div>
+                <div class="flex">
+                    <div class="font-bold text-4xl strikethroughLink inverse">#copyright...</div>
+                </div>
                 <div class="mt-2">You know that standard text bit... <div class="italic inline font-bold">copyright &copy; {{ date('Y') }} all rights reserved</div> blah blah..... it does not apply around here 🤣, because even if all the content here belongs to me, I cannot stop you from sharing it, copying the text (even though... why would you copy a text that is about my personal life?) and so on... this is a democratic blog. Do whatever you want. Until I won't let you anymore.</div>
             </div>
         </div>
         <div class="bg-blue-200 w-full md:w-1/3 md:ml-4 mt-4 md:mt-0" x-data="{show: 'dog'}">
             <div class="p-4 text-center">
-                <h1>Please, meet...</h1>
+                <div class="flex justify-center pb-2">
+                    <h1 class="strikethroughLink">#meet...</h1>
+                </div>
                 <template x-if="show == 'dog'">
-                    <div>Naba, our dog</div>
+                    <div>@Naba, our dog</div>
                 </template>
                 <template x-if="show == 'cat'">
-                    <div>Pounce - "the cat".</div>
+                    <div>@Tom & @Ginger, our cats.</div>
                 </template>
-                <div class="cursor-pointer bg-green-400 p-1 shadow-box hover:shadow-boxhvr inline-block" @click="show = 'cat'" :class="{hidden: show == 'cat'}">Let me see your cat!</div>
+                <div class="cursor-pointer bg-green-400 p-1 shadow-box hover:shadow-boxhvr inline-block" @click="show = 'cat'" :class="{hidden: show == 'cat'}">Let me see your cats!</div>
                 <div class="cursor-pointer bg-green-400 p-1 shadow-box hover:shadow-boxhvr inline-block" @click="show = 'dog'" :class="{hidden: show == 'dog'}">I want the dog again!</div>
             </div>
             <template x-if="show == 'dog'">
-                    <img src="{{get_stylesheet_directory_uri().'/resources/naba.jpeg'}}" alt="Naba, cainele meu." class="w-full">
-                </template>
-                <template x-if="show == 'cat'">
-                    <img src="{{get_stylesheet_directory_uri().'/resources/pounce.jpeg'}}" alt="Pounce, the \"matza\""." class="w-full">
-                </template>
+                <img src="{{get_stylesheet_directory_uri().'/resources/naba.jpeg'}}" alt="Naba, my doggo."
+                     class="w-full">
+            </template>
+            <template x-if="show == 'cat'">
+                <img src="{{get_stylesheet_directory_uri().'/resources/tomginger.jpg'}}" alt="Tom and Ginger..."
+                     class="w-full">
+            </template>
+            <template x-if="show == 'cat'">
+                <p class="px-4 py-2 text-sm">
+                    We had 5 cats in total. Two left and are wandering somewhere in the neighbourhood, one died :(.
+                    If the other 2 come back I will publish a photo of them too. If not... they don't deserve it!</p>
+            </template>
         </div>
     </div>
--- resources/views/generic/home_ro.blade.php
+++ resources/views/generic/home_ro.blade.php
@@ -1,7 +1,10 @@
+<div class="flex justify-center text-center pb-8">
+    <h1 class="strikethroughLink text-white text-5xl">Salutare 👋!</h1>
+</div>
 <div class="flex flex-col md:flex-row">
     <div class="bg-white w-full md:w-1/3">
-        <div class="p-2 text-center">
-            <h1>Salutare 👋!</h1>
+        <div class="p-2 flex">
+            <h1 class="strikethroughLink inverse">#despreMine</h1>
         </div>
         <div @touchstart="$refs.hi.play()" @touchend="$refs.hi.pause();" @mouseover="$refs.hi.play()"
              @mouseout="$refs.hi.pause();">
@@ -18,7 +21,10 @@
         </div>
     </div>
     <div class="bg-yellow w-full md:w-2/3 md:ml-4 mt-4 md:mt-0 flex flex-col p-4">
-        <div class="text-2xl w-full text-center">Reclama sponsorizata <small class="text-xs">de mine...</small></div>
+        <div class="w-full flex justify-center">
+            <div class="text-2xl strikethroughLink black">#reclamaSponsorizata <small class="text-xs">de mine...</small>
+            </div>
+        </div>
         <div>
             <p>Hey, <span class="text-2xl font-bold">tu</span>! Da, despre tine vorbesc! Ia fi atent putin aici.</p>
             <p class="mt-2">Asa, acum ca ti-am captat atentia cu tehnicile mele superbe de marketing, hai sa vorbim
@@ -26,6 +32,13 @@
             <p class="mt-2">De vreo 10 ani de zile sunt programator full time. Am fost destul de tocilar inca din
                 gimnaziu si liceu incat in timpul liber sa exersez programarea web, astfel incat am acumulat ceva
                 experienta.</p>
+            <p class="mt-2">
+                Momentan colaborez full-time cu super-firma din Romania numita <a href="https://www.arnia.com/"
+                                                                                  class="strikethroughLink inverse black">Arnia
+                    Software</a>.
+                Asta inseamna ca nu mai am timp extra de proiecte pentru alti clienti in aceasta perioada. Totusi, uite
+                cateva din tehnologiile cu care lucrez eu:
+            </p>
             <p class="mt-4">
                 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#php</span>
                 <span
@@ -48,15 +61,17 @@
                 <span
                     class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#javascript</span>
                 <span class="text-grey-200 text-xs hover:bg-red-400 hover:text-white hover:underline ml-1">#mysql</span>
-                <span class="text-base">sunt doar cateva din tehnologiile pe care daca nu esti tehnic sunt 10% sanse sa le fi auzit, dar eu le stapanesc si lucrez zilnic cu ele.
-Nu exista totusi solutia pefecta, de aceea in orele de consultanta vom cauta impreuna cea mai buna solutie pentru
-proiectul tau.
-</span>
+
             </p>
-            <p class="mt-2">Lucrez cu clienti din toata lumea, Canada, USA si desigur, Romania. Asa ca toti sunteti
-                bineveniti.</p>
+            <p class="mt-2">Daca nu esti tehnic, sunt putine sanse sa fi auzit ce multe din tehnologiile de mai sus.
+                Totusi, din experienta mea de 10+ ani de zile, nu exista solutia si tehnologia perfecta.
+                Fiecare proiect are particularitatile lui, deci nu spun niciodata nu unei tehnologii noi.
+            </p>
+            <p class="mt-2">Lucrez cu clienti din toata lumea, Canada, USA si desigur, Romania.</p>
             <p class="mt-2">Daca vrei sa facem impreuna un proiect IT care sa nu fie <strong>boring</strong>, ma poti
-                contacta pe <a href="mailto:filip@pacurar.dev">filip@pacurar.dev</a> sa intram in legatura.</p>
+                contacta pe <a href="mailto:filip@pacurar.dev">filip@pacurar.dev</a> sa intram in legatura. Tine cont de
+                faptul ca nu sunt disponibil
+                pentru programat decat in cazuri exceptionale, tot ce pot oferi momentan este consultanta IT.</p>
             <p class="mt-4 py-12 text-center w-fulL">
                 <a href="/despre-mine/consultanta-it" class="btn-cta-consultanta">Vreau consultanta IT</a>
             </p>
@@ -73,14 +88,21 @@ proiectul tau.
             una in 19 septembrie, automat streak-ul este 2
         </li>
         <li>Streak-ul se reseteaza automat atunci cand ultima postare nu este din ziua curenta sau anterioara</li>
+        <li>Se pun la socoteala postarile publice sau private (tot pe blog, dar nu le puteti vedea, sunt doar pentru
+            mine)
+        </li>
+        <li>Nu conteaza daca e postare in engleza sau in romana</li>
     </ul>
     <p class="mt-2">Codul sursa pentru calcularea acestui challenge este <a
-            href="https://github.com/filipac/pacurar.net-2021/blob/master/app/Jobs/CalculateStreak.php" target="_blank" class="strikethroughLink inverse">aici</a>
+            href="https://github.com/filipac/pacurar.net-2021/blob/master/app/Jobs/CalculateStreak.php" target="_blank"
+            class="strikethroughLink inverse">aici</a>
     </p>
     <div class="flex flex-col lg:flex-row w-full border-8 border-yellow mt-4">
-        <div class="flex-1 flex flex-col items-center justify-center p-10 border-b-8 lg:border-r-8 lg:border-b-0 border-yellow">
+        <div
+            class="flex-1 flex flex-col items-center justify-center p-10 border-b-8 lg:border-r-8 lg:border-b-0 border-yellow">
             <div class="text-2xl font-bold">Streak-ul current</div>
-            <div class="text-6xl font-bold mt-4">{{ ($streak = get_option('current_daily_streak_100d')) }} {{ $streak == 0 ? '😢' : '' }} {{ $streak != 0 && $streak < 5 ? '😄' : '' }} {{ $streak != 0 && $streak > 5 ? '💪' : '' }}</div>
+            <div
+                class="text-6xl font-bold mt-4">{{ ($streak = get_option('current_daily_streak_100d')) }} {{ $streak == 0 ? '😢' : '' }} {{ $streak != 0 && $streak < 5 ? '😄' : '' }} {{ $streak != 0 && $streak > 5 ? '💪' : '' }}</div>
         </div>
         <div class="flex-1 flex flex-col items-center justify-center p-10">
             <div class="text-2xl font-bold">Cel mai bun streak</div>
@@ -95,7 +117,9 @@ proiectul tau.
     <div class="flex flex-col w-full md:w-2/3">
         <div class="bg-pink-200">
             <div class="p-2 pb-4">
-                <h1>Easter-egg hunt!</h1>
+                <div class="flex">
+                    <h1 class="strikethroughLink inverse">#easterEggHunt!</h1>
+                </div>
                 <p>Pentru ca nu este un blog plicticos, am ascuns cateva lucruri interactive in acest blog. Ce zici,
                     poti sa le gasesti pe toate? Sincer nici eu nu stiu cate sunt, pentru ca le fac pe loc, cum imi vine
                     o idee noua ciudata, cum o implementez.</p>
@@ -110,7 +134,9 @@ proiectul tau.
         </div>
         <div class="bg-purple-300 mt-4">
             <div class="p-2 pb-4 text-center">
-                <div class="font-bold text-2xl">Ma poti urmari pe:</div>
+                <div class="flex justify-center">
+                    <div class="font-bold text-2xl strikethroughLink">#urmaresteMa:</div>
+                </div>
                 <div class="font-bold text-xs">(chiar daca esti FBI sau ceva, nu am nimic de ascuns)</div>
                 <div class="flex flex-col md:flex-row items-center justify-center mt-4">
                     <div>
@@ -147,7 +173,9 @@ proiectul tau.
             </div>
         </div>
         <div class="bg-green-400 mt-4 p-4 flex-1">
-            <div class="font-bold text-4xl">Copyright...</div>
+            <div class="flex">
+                <div class="font-bold text-4xl strikethroughLink inverse">#copyright...</div>
+            </div>
             <div class="mt-2">Stiti voi tot textul ăla standard...
                 <div class="italic inline font-bold">copyright &copy; {{ date('Y') }} toate drepturile rezervate</div>
                 bla bla..... aici nu prea se aplica :)), pentru ca desi tot continutul imi apartine, nu va pot opri sa
@@ -158,15 +186,17 @@ proiectul tau.
     </div>
     <div class="bg-blue-200 w-full md:w-1/3 md:ml-4 mt-4 md:mt-0" x-data="{show: 'dog'}">
         <div class="p-4 text-center">
-            <h1>Faceti cunostinta cu...</h1>
+            <div class="flex justify-center pb-2">
+                <h1 class="strikethroughLink">#facetiCunostintaCu...</h1>
+            </div>
             <template x-if="show == 'dog'">
-                <div>Naba, cainele nostru.</div>
+                <div>@Naba, cainele nostru.</div>
             </template>
             <template x-if="show == 'cat'">
-                <div>Pounce - "the cat".</div>
+                <div>@Tom & @Ginger, pisicile noastre.</div>
             </template>
             <div class="cursor-pointer bg-green-400 p-1 shadow-box hover:shadow-boxhvr inline-block"
-                 @click="show = 'cat'" :class="{hidden: show == 'cat'}">Vreau sa vad pisica!
+                 @click="show = 'cat'" :class="{hidden: show == 'cat'}">Vreau sa vad pisicile!
             </div>
             <div class="cursor-pointer bg-green-400 p-1 shadow-box hover:shadow-boxhvr inline-block"
                  @click="show = 'dog'" :class="{hidden: show == 'dog'}">Vreau sa vad iar cainele!
@@ -177,8 +207,12 @@ proiectul tau.
                  class="w-full">
         </template>
         <template x-if="show == 'cat'">
-            <img src="{{get_stylesheet_directory_uri().'/resources/pounce.jpeg'}}" alt="Pounce, the \" matza\""."
-            class="w-full">
+            <img src="{{get_stylesheet_directory_uri().'/resources/tomginger.jpg'}}" alt="Tom and Ginger..."
+                 class="w-full">
+        </template>
+        <template x-if="show == 'cat'">
+            <p class="px-4 py-2 text-sm">Am avut in total 5 pisici. Doua au plecat si sunt undeva prin cartier, una a
+                murit din pacate. Daca revin cele 2, pun poza si cu ele. Daca nu... nu merita!</p>
         </template>
     </div>
 </div>
