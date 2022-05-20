ad8b8b25d833230d804892fbe9f7ca2a0e859fd5
Filip Pacurar <filip@pacurar.net>
2022-05-20 17:24:45 +0300

    wording + case

--- resources/views/partials/copy.blade.php
+++ resources/views/partials/copy.blade.php
@@ -3,11 +3,11 @@
     @if(ICL_LANGUAGE_CODE == 'ro')
     <div class="mt-2">Copyright &copy; {{ date('Y') }} toate drepturile <div class="italic inline font-bold">nu sunt</div> rezervate. Faceti ce vreti, e o tara libera.</div>
     <div class="mt-2 text-xs">Cred ca nu mai are rost sa zic, dar tema e facuta de mine cu TailwindCSS. Gasesti codul sursa <a href="https://github.com/filipac/pacurar.net-2021" target="_blank" class="underline font-bold">aici</a>.</div>
-    <div class="mt-2 text-xs">Din pacate, inca folosesc Wordpress. Tema e insa custom Laravel 😎.</div>
+    <div class="mt-2 text-xs">Inca folosesc WordpPess 🧡. Tema e insa custom Laravel 😎.</div>
     @else
     <div class="mt-2">Copyright &copy; {{ date('Y') }} all rights <div class="italic inline font-bold">are not</div> reserved. Do whatever you want, it's a free country.</div>
     <div class="mt-2 text-xs">Guess it's obvious, but the theme is created by myself with  TailwindCSS. You can find the source code <a href="https://github.com/filipac/pacurar.net-2021" target="_blank" class="underline font-bold">here</a>.</div>
-    <div class="mt-2 text-xs">Sadly, I still use Wordpress. The theme is custom Laravel though 😎.</div>
+    <div class="mt-2 text-xs">I still use WordPress 🧡. The theme is custom Laravel though 😎.</div>
     @endif
 </div>
 </div>
