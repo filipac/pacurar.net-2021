644a8b46e339fc2228d0fdc1404e02eae2ef6585
Filip Pacurar <filip@pacurar.net>
2023-02-15 17:57:19 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+import react from '@vitejs/plugin-react'
+
+import {dependencies} from './package.json';
+
+function renderChunks(deps) {
+    let chunks = {};
+    Object.keys(deps).forEach((key) => {
+        if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+        chunks[key] = [key];
+    });
+    return chunks;
+}
 
 export default defineConfig({
     plugins: [
@@ -7,18 +20,22 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/react-app.tsx',
             ],
             valetTls: 'blog.test',
-            // refresh: true,
+            refresh: true,
         }),
+        react(),
     ],
-    experimental: {
-        renderBuiltUrl(filename, {hostType}) {
-            if (hostType === 'js') {
-                return {runtime: `window.__toCdnUrl(${JSON.stringify(filename)})`}
-            } else {
-                return {relative: true}
-            }
-        }
-    }
+    // build: {
+    //     sourcemap: false,
+    //     rollupOptions: {
+    //         output: {
+    //             manualChunks: {
+    //                 vendor: ['react', 'react-router-dom', 'react-dom'],
+    //                 ...renderChunks(dependencies),
+    //             },
+    //         },
+    //     },
+    // },
 });
