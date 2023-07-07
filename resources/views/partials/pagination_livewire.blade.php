a1f56e20f04ff8378d7e07daae4f65a601b9b3cb
Filip Pacurar <filip@pacurar.net>
2023-07-07 20:12:18 +0300

    fix pagination mobile

--- resources/views/partials/pagination_livewire.blade.php
+++ resources/views/partials/pagination_livewire.blade.php
@@ -8,33 +8,33 @@
         @php(isset($this->numberOfPaginatorsRendered[$paginator->getPageName()]) ? $this->numberOfPaginatorsRendered[$paginator->getPageName()]++ : $this->numberOfPaginatorsRendered[$paginator->getPageName()] = 1)
 
         <nav role="navigation" aria-label="Pagination Navigation" class="flex items-center justify-between">
-            <div class="flex justify-between flex-1 sm:hidden">
-                <span>
-                    @if ($paginator->onFirstPage())
-                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md select-none">
-                            {!! $t__('pagination.previous') !!}
-                        </span>
-                    @else
-                        <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {!! $t__('pagination.previous') !!}
-                        </button>
-                    @endif
-                </span>
+{{--            <div class="flex justify-between flex-1 sm:hidden">--}}
+{{--                <span>--}}
+{{--                    @if ($paginator->onFirstPage())--}}
+{{--                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md select-none">--}}
+{{--                            {!! $t__('pagination.previous') !!}--}}
+{{--                        </span>--}}
+{{--                    @else--}}
+{{--                        <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">--}}
+{{--                            {!! $t__('pagination.previous') !!}--}}
+{{--                        </button>--}}
+{{--                    @endif--}}
+{{--                </span>--}}
 
-                <span>
-                    @if ($paginator->hasMorePages())
-                        <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {!! $t__('pagination.next') !!}
-                        </button>
-                    @else
-                        <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md select-none">
-                            {!! $t__('pagination.next') !!}
-                        </span>
-                    @endif
-                </span>
-            </div>
+{{--                <span>--}}
+{{--                    @if ($paginator->hasMorePages())--}}
+{{--                        <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">--}}
+{{--                            {!! $t__('pagination.next') !!}--}}
+{{--                        </button>--}}
+{{--                    @else--}}
+{{--                        <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md select-none">--}}
+{{--                            {!! $t__('pagination.next') !!}--}}
+{{--                        </span>--}}
+{{--                    @endif--}}
+{{--                </span>--}}
+{{--            </div>--}}
 
-            <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+            <div class="flex-1 flex items-center justify-between">
                 <div>
                     <p class="text-sm leading-5">
                         <span>{!! $t__('Showing') !!}</span>
