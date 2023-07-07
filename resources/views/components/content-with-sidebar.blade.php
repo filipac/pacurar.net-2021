d8b30cc7c88476e6b4cabbe874f01bdb0220acbd
Filip Pacurar <filip@pacurar.net>
2023-07-07 20:19:01 +0300

    fix

--- resources/views/components/content-with-sidebar.blade.php
+++ resources/views/components/content-with-sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-blog-list-mobile xl:grid-cols-blog-list gap-4">
+<div class="grid grid-cols-blog-list-mobile xl:grid-cols-blog-list md:gap-4">
     <div>
         {{ $slot }}
     </div>
