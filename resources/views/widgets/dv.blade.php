1d166c6b7e1d4dbe4469ee585db1c79c56002f9f
Filip Pacurar <filip@pacurar.net>
2025-09-05 15:15:21 +0300

    DV

--- resources/views/widgets/dv.blade.php
+++ resources/views/widgets/dv.blade.php
@@ -1,5 +1,5 @@
 <x-widget :title="$title"
-          x-data="timer{{$attrs['id']}}(new Date(Date.UTC(2025, 6, 21, 14, 30, 0, 0)))"
+          x-data="timer{{$attrs['id']}}(new Date(Date.UTC(2026, 4, 2, 17, 30, 0, 0)))"
           x-init="init();"
 >
     <p class="text-xs">
@@ -7,12 +7,12 @@
             We started timidly, with low hopes but confident that if it's God's will, one day we will move to the United States of America.
             We participated in the visa lottery for 4 consecutive years and on May 4th,
             2024 we found out that we were selected for a possible immigrant visa in 🇺🇸.
-            We owe everything to God, He will help us until the end.
+            Unfortunately, even though we were selected, in 2025 fiscal year they stopped at the case number 23.000 for Europe, and we had 29.000. So we did not get a green card... we'll try again for DV2027.
         @else
             Am inceput timid, cu sperante mici dar increzatori ca daca este voia lui Dumnezeu intr-o zi vom ajunge
             sa ne mutam in Statele Unite ale Americii. Am participat la loteria vizelor 4 ani consecutiv
             si in 4 mai 2024 am aflat ca am fost alesi pentru o posibila viza de imigrant in 🇺🇸.
-            Lui Dumnezeu ii datoram totul, El ne va ajuta pana la capat.
+            Din pacate, chiar dacaam fost alesi, in anul fiscal 2025 s-au oprit la numarul de caz 23.000 pentru Europa, iar noi aveam 29.000. Deci nu am primit green card... vom incerca din nou pentru DV2027
         @endif
     </p>
     <ul>
@@ -30,12 +30,15 @@
         </li>
         <li>
             <strong>DV-2025</strong> -
-            ✅ {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'We were amongst the 1% selected' : 'Am fost printre cei 1% selectati' }}
+            ❌ {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'We were amongst the 1% selected, but did not get a green card...' : 'Am fost printre cei 1% selectati, dar nu am primit green card...' }}
         </li>
         <li>
-            <strong>
-                🕧 {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'Interview ETA' : 'Data interviului (approx)' }}
-            </strong> - 🕧 {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'Left: ' : 'A rams: ' }}
+            <strong>DV-2026</strong> -
+            ❌ {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'Again not selected...' : 'Din nou nu am fost selectati...' }}
+        </li>
+
+        <li>
+            <strong>DV-2027</strong> - 🕧 {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'We find out in ' : 'Aflam in ' }}
             <span x-text="time().days"></span>
             <span>{{ ICL_LANGUAGE_CODE == 'ro' ? 'zile' : 'days' }},</span>
             <span x-text="time().hours"></span>
@@ -45,19 +48,8 @@
             <span>{{ ICL_LANGUAGE_CODE == 'ro' ? 'si' : 'and' }} </span>
             <span x-text="time().seconds"></span>
             <span>{{ ICL_LANGUAGE_CODE == 'ro' ? 'secunde' : 'seconds' }}</span>
-            <ul>
-                <li>
-                    🏥{{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'Medical' : 'Vizita medicala' }}
-                </li>
-                <li>
-                    👮🏻‍{{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'Interview' : 'Interviul' }}
-                </li>
-                <li>
-                    ✈ {{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'Flight' : 'Zborul' }}
-                </li>
-            </ul>
+            <strong>({{ defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'en' ? 'application in October 2025, results in May 2nd 2026' : 'aplicam in octombrie 2025, rezultatele sunt in 2 mai 2026' }})</strong>
         </li>
-    </ul>
 </x-widget>
 <script>
     function timer{{$attrs['id']}}(expiry) {
