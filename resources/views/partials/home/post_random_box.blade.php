48bf51b17a50b356be510aed0f968ea1247c24cd
Filip Pacurar <filip@pacurar.net>
2026-07-31 12:34:10 +0200

    Add conditional random box selection based on request parameter

--- resources/views/partials/home/post_random_box.blade.php
+++ resources/views/partials/home/post_random_box.blade.php
@@ -1,5 +1,8 @@
 @php
     $random = collect([1,2,3,3])->random();
+    if(request()->has('randombox')) {
+        $random = request()->get('randombox');
+    }
     //$random = 4;
 @endphp
 @include('partials.home.random_'.$random)
